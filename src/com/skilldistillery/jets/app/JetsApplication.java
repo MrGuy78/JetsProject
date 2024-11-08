c5a0125d689778dd41025c041b1e60f06fd31b64
Andrew Nelson <nels4766@gmail.com>
2024-11-08 14:40:39 -0700

    Stub application and AirField classes

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -1,9 +1,33 @@
 package com.skilldistillery.jets.app;
 
+import java.util.Scanner;
+
 public class JetsApplication {
 
+	private Airfield airfield = new Airfield();
+	private static Scanner sc;
+	
+	//NO other fields; NO list of jets
+	
 	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		JetsApplication app = new JetsApplication();
+		app.launch();
+		
+		
+		sc.close();
 
+	}
+	
+	private void launch() {
+		//loop
+		//displayUserMenu
+		//get choice from scanner
+		//act on user choice
+	}
+	
+	private void displayUserMenu() {
+		//sysouts for menu
 	}
 
 }
